d2097a63a46b704b4bc5805281e2405e6a13b315
cnugroho11 <cnugroho211@gmail.com>
2022-06-12 15:53:06 +0700

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
